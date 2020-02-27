d27ca73768e0e0a59968d12f7c59ec6635ed70a0
vmwarechina <hus@vmware.com>
2020-02-27 21:50:10 +0800

    remove logo in app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>wtf you talking about</h3>
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
